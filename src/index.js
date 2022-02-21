fda0d8550106e3a9be10ac5fc635f6fafd88e64c
Vitalii Peresoliak <vitalik21022@gmail.com>
2022-02-21 18:39:04 +0200

    try to fixed bug

--- src/index.js
+++ src/index.js
@@ -13,15 +13,15 @@ const inputEl = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
 
+
 const onCountriesFetch = (event) => {
     const inputValue = event.target.value.trim();
-    if (inputValue.length === 0) {
-        countryListEl.innerHTML = "";
-        return;
+    if (inputValue.length === "") {
+        return clearAll();
     } else {
         fetchCountries(inputValue).then(markupCountryInfo).catch(() => {
-            countryListEl.innerHTML = "";
             Notify.failure('Oops, there is no country with that name');
+            clearAll();
         });
     };
 }
@@ -30,11 +30,16 @@ inputEl.addEventListener('input', debounce(onCountriesFetch, DEBOUNCE_DELAY));
 
 const markupCountryInfo = (country) => {
     if (country.length > 10) {
-        countryInfoEl.innerHTML = "";
+        clearAll();
         return Notify.info("Too many matches found. Please enter a more specific name.");
-    } if (country.length === 1) {
-        countryInfoEl.innerHTML = countryInfo(country[0]);
-    } else {
+    } if (country.length >= 2 && country.length <= 10) {
         countryListEl.innerHTML = countryList(country);
+    } else {
+        countryInfoEl.innerHTML = countryInfo(country[0]);
     }
+}
+
+function clearAll() {
+    countryListEl.innerHTML = '';
+    countryInfoEl.innerHTML = '';
 }
